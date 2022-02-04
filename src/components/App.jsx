5de5b2734ba84c4e6c4e8f075c03b40d1ae63cf0
Zitral12 <67572527+Zitral12@users.noreply.github.com>
2022-02-03 18:08:04 -0800

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,17 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  var questionName = 0;
+  return (
+    <div className="app">
+      Trivia!
+      <Question question="Question Goes Here" />
+    </div>
+  );
+}
+
+function Question(props) {
+  return <div>{props.question}</div>;
 }
 
 export default App;
